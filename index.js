ff3395d9fb343ddd146d4e08db942093d14045f6
zprs <41876155+zprs@users.noreply.github.com>
2020-06-30 21:58:56 -0700

    changed to puppeteer core

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const puppeteer = require('puppeteer-extra');
+const puppeteer = require('puppeteer-core');
 // const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 // puppeteer.use(StealthPlugin())
 
